9557555d7eb63dc5cc95b50a43b5b8494832698c
Gopal Saraswat <gopalsaraswat96@.com>
2026-05-06 23:30:19 +0530

    maincodeupdatd

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "rg"{
   name = "demorg"
   location = "eastus"
 }
+
+resource "azurerm_virtual_netowork" "vnet"{
+    name="demovnet"
+    resource_group_name=azurerm_resource_group.rg.name
+    location=azurerm_resource_group.rg.location
+    address_space=["10.0.0.0/16"]
+}
