e486022ba9c520331609c5bbd61e270507523db9
gopal19931 <159487919+gopal19931@users.noreply.github.com>
2026-05-07 08:32:46 +0530

    this is fetch testing.

--- main.tf
+++ main.tf
@@ -19,3 +19,6 @@ resource "azurerm_storage_account" "strg"{
   resource_group_name = azurerm_resource_group.rg.name
   location = azurerm_resource_group.rg.location
 }
+
+# this is the comment code
+
